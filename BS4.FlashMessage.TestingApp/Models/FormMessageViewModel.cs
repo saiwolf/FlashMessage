edb8335fb3f37f783c554f5cf51c0fb94a2e34f2
Robert Cato <rcato@ci.thibodaux.la.us>
2021-08-06 16:08:14 -0500

    Updated Testing App folders to use proper namespace names

---
+++ BS4.FlashMessage.TestingApp/Models/FormMessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SWMNU.NET.BS4.FlashMessage.TestingApp.Models
+{
+    public class FormMessageViewModel
+    {
+        [Required(ErrorMessage = "First Name is required!")]
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required!")]
+        [Display(Name = "Last Name:")]
+        public string LastName { get; set; }
+    }
+}

5539dc5177ada70744742019193e520d4b761970
Robert Cato <rcato@ci.thibodaux.la.us>
2021-08-06 15:49:16 -0500

    Created BS5 Testing App

---
+++ FlashMessage.BS5.TestingApp/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SWMNU.NET.BS5.FlashMessage.TestingApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

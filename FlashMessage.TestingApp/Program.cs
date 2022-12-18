324700eb4ceac19a28fe7768d1be8de6f738fa3c
Robert E. Cato <robertcato015@gmail.com>
2022-12-18 13:03:00 -0600

    Converted Testing App to use Top Level statements, Implicit Usings, and appsettings.json for Serilog configuration

--- FlashMessage.TestingApp/Program.cs
+++ FlashMessage.TestingApp/Program.cs
@@ -1,41 +1,69 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Serilog;
-using Serilog.Events;
 
-namespace SWMNU.NET.FlashMessage.TestingApp
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateBootstrapLogger();
+
+Log.Information($"Starting application at {DateTime.UtcNow}");
+
+try
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.FromLogContext()
-                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-                .WriteTo.Console()
-                .CreateLogger();
-            try
-            {
-                Log.Information("Starting Web Host...");
-                host.Run();
-            }
-            catch
-            {
-                Log.Fatal("Host terminated unexpectedly!");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    IServiceCollection? services = builder.Services;
+    IWebHostEnvironment environment = builder.Environment;
+
+    services.AddControllersWithViews();
+    services.AddRazorPages();
+
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "test", Version = "v1" });
+    });
+
+    WebApplication app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "test");
+        });
     }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.MapControllers();
+    app.MapRazorPages();
+    app.Run();
+}
+catch (Exception ex)
+{
+    if (ex.GetType().Name.Equals("StopTheHostException", StringComparison.Ordinal))
+        throw;
+    Log.Fatal("Unhandled Error. Shutting down.");
+}
+finally
+{
+    Log.Information($"Shutting down app at {DateTime.UtcNow}");
+    Log.CloseAndFlush();
 }
